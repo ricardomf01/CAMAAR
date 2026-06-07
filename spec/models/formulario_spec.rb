2b7e06ce83d21108eaaf682661d32b2a373f05ed
Hugo Macedo <hugomacedoramos@gmail.com>
2026-06-07 14:17:55 -0300

    test: adiciona especificacoes do rspec em estado falho para a issue #103

--- spec/models/formulario_spec.rb
+++ spec/models/formulario_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Formulario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'validates presence of publico_alvo' do
+      formulario = Formulario.new(publico_alvo: nil)
+      expect(formulario).not_to be_valid
+      expect(formulario.errors[:publico_alvo]).to include("Obrigatório: Selecione se o formulário é destinado a docentes ou discentes.")
+    end
+
+    it 'validates data_limite is after data_inicio' do
+      formulario = Formulario.new(data_inicio: Time.now, data_limite: 1.day.ago)
+      formulario.valid?
+      expect(formulario.errors[:data_limite]).to include("deve ser posterior à data de início")
+    end
+  end
 end
