194566eb3dac0ff36494b0906d7384f5553e46e0
Leanddro13 <leanddrosouzasilva@gmail.com>
2026-06-07 13:56:02 -0300

    feat: implementa a funcionalidade completa da issue #106

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # Admin special department actions
+  get "admin/turmas" => "admin#turmas", as: :admin_turmas
+  get "admin/turmas/:id/avaliacoes" => "admin#turma_avaliacoes", as: :admin_turma_avaliacoes
+  get "admin/desempenho_semestral" => "admin#desempenho_semestral", as: :admin_desempenho_semestral
 
   # Admin CSV Report
   get "admin/relatorios/csv" => "resultados#export_csv_resultado", as: :admin_relatorios_csv
