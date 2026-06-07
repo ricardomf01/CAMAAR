f34831a55209d53528d927bd5b352e0f0bbbc84c
Hugo Macedo <hugomacedoramos@gmail.com>
2026-06-07 14:53:09 -0300

    feat: implementa a funcionalidade completa da issue #109

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,28 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= Usuario.find_by(id: session[:usuario_id]) if session[:usuario_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "Você precisa estar logado para acessar esta página."
+      redirect_to login_path
+    end
+  end
+
+  def require_admin
+    unless logged_in? && current_user.perfil == "administrador"
+      flash[:alert] = "Acesso restrito a administradores."
+      redirect_to root_path
+    end
+  end
 end
