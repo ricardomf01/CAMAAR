62e5ba7dad0f5472d90f4f508ba0cbccc26779fa
ineblinavel <luanribeirosant@gmail.com>
2026-06-07 10:47:11 -0300

    feat: implementa a funcionalidade completa da issue #104

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+  before_action :require_admin
+
+  def dashboard
+    # Stub action for login redirection
+  end
+end
---
+++ app/controllers/avaliacoes_controller.rb
@@ -0,0 +1,7 @@
+class AvaliacoesController < ApplicationController
+  before_action :require_user
+
+  def index
+    # Stub action for login redirection
+  end
+end
