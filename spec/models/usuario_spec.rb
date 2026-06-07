7223774f89340a8f18888db8b2f46d5bd25f7352
ineblinavel <luanribeirosant@gmail.com>
2026-06-07 09:53:42 -0300

    test: adiciona cenarios de aceitacao e unitarios falhos para a issue #104

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Usuario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      usuario = Usuario.new(nome: 'Test User', email: 'test@unb.br', perfil: 'discente', ativo: true)
+      usuario.password = '123456'
+      expect(usuario).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      usuario = Usuario.new(nome: 'Test', email: nil, perfil: 'discente')
+      expect(usuario).not_to be_valid
+    end
+  end
 end
